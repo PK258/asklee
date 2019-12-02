144e576bc947f7e110ced13bd0012db6c6ee500e
PK258 <pradeepu258@gmail.com>
2019-12-02 17:01:38 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+
+
+read_data= """select start_date, start_location_latitude, start_location_longitude, 
+                        end_location_latitude, end_location_longitude, start_location, end_location 
+                        from smarte_app.trip where start_location_latitude is not null;"""
+
+
+mapbox_token = "pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoiY2pudzRtaWloMDAzcTN2bzN1aXdxZHB5bSJ9.2bkj3IiRC8wj3jLThvDGdA"
---
+++ plottingCoordinates.py
@@ -0,0 +1,55 @@
+import psycopg2
+import plotly.graph_objects as go
+import plotly
+import plotly.express as px
+import config
+
+conn = psycopg2.connect(
+    database="postgres",
+    user="postgres",
+    password="FebiacWoluwe2019",
+    host="dev-smepostgres.cpesf20fxopt.eu-central-1.rds.amazonaws.com",
+    port='5432'
+)
+cur = conn.cursor()
+cur.execute(config.read_data)
+rows = cur.fetchall()
+startLat = []
+startLon = []
+endLat = []
+endLon = []
+startLoc = []
+endLoc = []
+
+
+for row in rows:
+    startLat.append(row[1])
+    startLon.append(row[2])
+    endLat.append(row[3])
+    endLon.append(row[4])
+    startLoc.append(row[5])
+    endLoc.append(row[6])
+
+fig = go.Figure()
+routes = []
+px.set_mapbox_access_token(config.mapbox_token)
+
+for i in range(len(endLon)):
+    fig.add_trace(go.Scattermapbox(
+        mode="markers+lines",
+        lon=[startLon[i], endLon[i]],
+        lat=[startLat[i], endLat[i]],
+        showlegend=False,
+        marker={'size': 10},
+        text=[startLoc[i], endLoc[i]]))
+
+fig.update_layout(
+    mapbox_style="dark", mapbox_accesstoken=config.mapbox_token)
+
+fig.update_layout(
+    mapbox={
+        'center': {'lon': 4.4834, 'lat': 50.7528},
+        'style': "carto-darkmatter",
+        'zoom': 8})
+
+plotly.offline.plot(fig, filename='Asklee_paths.html')

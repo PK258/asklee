807573b4de1505e0426b925238921985d5b040cf
pk258 <pradeepu258@gmail.com>
2019-12-03 12:10:56 +0100

    Add files

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+from functions import fetch_plot_data
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def page():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def values():
+    try:
+        if request.method == 'POST':
+            for UserID, FromDate, ToDate in \
+                    zip(request.form.getlist('user_id'),
+                        request.form.getlist('from_date'),
+                        request.form.getlist('to_date')):
+                fetch_plot_data(UserID, FromDate, ToDate)
+                return render_template('Asklee_paths.html')
+            else:
+                return render_template('index.html')
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
